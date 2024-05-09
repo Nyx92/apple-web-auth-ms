1f41dc03b2d3eeed437f73ba8908917b144331c1
Nyx92 <ngyx92@gmail.com>
2024-05-10 01:24:14 +0800

    initialize project

---
+++ src/test/java/apple/web/authms/AuthMsApplicationTests.java
@@ -0,0 +1,13 @@
+package apple.web.authms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
